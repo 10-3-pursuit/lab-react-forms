45245cc597904563eec796385581fcc73283678b
Timothy Natal <timothynatal@pursuit.org>
2023-12-12 17:55:07 -0500

    Finished lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <Form/>
     </main>
   );
 }
