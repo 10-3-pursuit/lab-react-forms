33aabd966f128048f40ee7ae99422082da4114bb
Carlitos <carlitosdutan@pursuit.org>
2023-12-12 22:59:59 -0500

    main functionality completed

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,78 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
+
+  const [inputValue, setInputValue] = useState({
+    numbers:"",
+    operation:""
+  });
+
+  const [total, setTotal] = useState(0);
+
+  function handleChange(event) {
+    setInputValue({...inputValue, [event.target.id]: event.target.value});
+  }
+
+  function calculateTotal() {
+    // convert numbers into an array
+    const arr = inputValue.numbers.split(",").map(Number);
+
+    if(inputValue.operation === "sum"){
+      const sum = arr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, 0);
+      setTotal(sum)
+    }
+    
+    else if(inputValue.operation === "average"){
+      const sum = arr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      },0);
+      const average = sum/arr.length || 0;
+      setTotal(average);
+    }
+    else if(inputValue.operation === "mode"){
+      const mode = calculateMode();
+      setTotal(mode);
+    }
+  }
+
+  function calculateMode() {
+    const arr = inputValue.numbers.split(",").map(Number);
+  
+    const frequencyMap = {};
+    let maxFrequency = 0;
+    let mode = [];
+  
+    arr.forEach((number) => {
+      frequencyMap[number] = (frequencyMap[number] || 0) + 1;
+      if (frequencyMap[number] > maxFrequency) {
+        maxFrequency = frequencyMap[number];
+        mode = [number];
+      } else if (frequencyMap[number] === maxFrequency) {
+        mode.push(number);
+      }
+    });
+  
+    if (mode.length === arr.length) {
+      return "No mode";
+    }
+  
+    return mode.join(", ");
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    calculateTotal();
+  }
+  
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} value={inputValue.numbers} id="numbers" name="numbers" type="text" />
+        <select onChange={handleChange} value={inputValue.operation} id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -15,7 +81,7 @@ function Form() {
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{total}</p>
       </section>
     </>
   );
