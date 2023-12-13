f88417bb4bd5a1045ad2d2775e04f1baab7b8712
KenCab123 <KennethCabral@pursuit.org>
2023-12-12 20:07:37 -0500

    I realized i did it wrong

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
-
-const calculate = (event, values) => {
-  event.preventDefault();
-  values = event.target.values.value
-  const op = document.getElementById("operation")
-
-  const removedComma = ','
-  let numArr = values.split(removedComma).map(Number);
-
-  const result = document.getElementById("result")
-  const p = result.querySelector("p") 
-  
-  const sum = numArr.reduce((acc, curr) => acc + curr,0)
-
-  switch (op.value) {
-    case 'sum':
-      if(!isNaN(sum)) {
-        p.innerHTML = sum
-      } else {
-        p.innerHTML = `Invalid Input`
-      }
-    break;
-    case 'average':
-      if(!isNaN(sum)) {
-        p.innerHTML = sum / numArr.length
-      } else {
-        p.innerHTML = `Invalid Input`
-      }
-    break;
-    case 'mode':
-      let numCount = {}
-      for(let i = 0; i < numArr.length; i++) {
-        const num = parseFloat(numArr[i])
-        if (!isNaN(num)) {
-        numCount[num] = (numCount[num] || 0) + 1;
-        } else {
-          return p.innerHTML = `Invalid Input`
-        }
-      }
-
-      let maxCount = 0;
-      let maxOccurence;
-      for(const num in numCount) {
-        if(numCount[num] > maxCount) {
-          maxCount = numCount[num]
-          maxOccurence = num 
-        }
-      }
-
-      p.innerHTML = maxOccurence
-    break;
-    default:
-      p.innerHTML = `Invalid Input`
-    break;
-  }
-}
-
-const reset = (event) => {
-  values = event.target.values.value
-  console.log(values)
-}
-
 function App() {
+  const [valueAndOp, setValueAndOp] = useState({
+    value: [],
+    operation: ""
+  });
+
+  const [result, setResult] = useState(null);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const calculation = calculate();
+    setResult(calculation);
+  };
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setValueAndOp((prevValue) => ({
+      ...prevValue,
+      value: inputValue.split(",").map((val) => val.trim()),
+    }));
+  };
+
+  const handleOpChange = (e) => {
+    setValueAndOp({
+      ...valueAndOp,
+      operation: e.target.value,
+    });
+  };
+
+  const calculate = () => {
+    const value = valueAndOp.value;
+    const answer = document.getElementById("result");
+    const p = answer.querySelector("p");
+    const sum = value.map(Number).reduce((acc, curr) => acc + curr, 0);
+
+    switch (valueAndOp.operation) {
+      case 'sum':
+        if (!isNaN(sum)) {
+          p.innerHTML = sum;
+        } else {
+          p.innerHTML = `Invalid input.`;
+        }
+        break;
+      case 'average':
+        if (!isNaN(sum)) {
+          p.innerHTML = sum / value.length;
+        } else {
+          p.innerHTML = `Invalid input.`;
+        }
+        break;
+      case 'mode':
+        let numCount = {};
+        let invalidInput = false;
+        for (let i = 0; i < value.length; i++) {
+          const num = value[i]
+          if (!isNaN(num)) {
+            numCount[num] = (numCount[num] || 0) + 1;
+          } else {
+            invalidInput = true;
+            break;
+          }
+        }
+        if (invalidInput) {
+          p.innerHTML = `Invalid input.`;
+          break;
+        }
+        let maxCount = 0;
+        let maxOccurrence;
+        for (const num in numCount) {
+          if (numCount[num] > maxCount) {
+            maxCount = numCount[num];
+            maxOccurrence = num;
+          }
+        }
+        p.innerHTML = maxOccurrence;
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form calculate={calculate} reset={reset}/>
+      <Form
+        submit={submit}
+        handleInputChange={handleInputChange}
+        handleOpChange={handleOpChange}
+        result={setResult}
+      />
     </main>
   );
 }
+
 
 export default App;
