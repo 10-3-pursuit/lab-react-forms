268462199e6037a45d983b6959a959d88328795e
Jennifer <jenniferpeterson@pursuit.org>
2023-12-13 00:44:21 -0500

    still working on this lab😮‍💨

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,65 @@
 import React from "react";
 import "./Form.css";
+import { useState } from "react";
 
 function Form() {
+  const [numInput, setNumInput] = useState("");
+  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // function for the change within the input box that turns the number into an array
+  const handleChange = (event) => {
+    setNumInput(event.target.value.split(","));
+  };
+
+  const handleOperation = (event) => {
+    // console.log(handleOperation);
+    setOperation(event.target.value);
+  };
+
+  // function for the calculate button
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  // calculation function
+  const calculate = (numInput, operator) => {
+    const filteredNums = numInput.filter((num) => num.trim() !== "");
+
+    if (
+      filteredNums.every(
+        (num) => typeof num !== "number" || !filteredNums.length
+      )
+    ) {
+      setErrorMessage("Invalid Input");
+    }
+
+    if (operator === "sum") {
+      result = filteredNums.reduce((acc, curr) => +acc + +curr, 0);
+    } else if (operator === "average") {
+      result =
+        filteredNums.reduce((acc, curr) => +acc + +curr, 0) /
+        filteredNums.length;
+    } else if (operator === "mode") {
+      let modeObj = {};
+      let mostFrequent = 0;
+
+      filteredNums.forEach((num) => {
+        if (modeObj[num]) {
+          modeObj[num]++;
+        } else {
+          modeObj[num] = 1;
+        }
+      });
+    }
+  };
+
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} id="values" name="values" type="text" />
+        <select onChange={handleOperation} id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -15,7 +68,7 @@ function Form() {
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </>
   );
