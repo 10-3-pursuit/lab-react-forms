cf100ffb7693095fa2d7bd5d855fbf31c56cf641
Iris Beckham <irisdancer3@gmail.com>
2023-12-15 13:47:12 -0800

    handleChange function

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,11 +1,26 @@
-import React from "react";
 import "./Form.css";
+import { useState } from "react";
 
 function Form() {
+
+  const [numbersArray, setNumbersArray] = useState([]);
+
+  function handleChange(e) {
+    setNumbersArray(e.target.value.split(','))
+    console.log(numbersArray)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // reset
+  }
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          // value={numbersArray}
+          id="values" name="values" type="text" />
         <select id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
