9a6ac4255c04f3f43e73531b6d463860ba9a748a
juligarc91 <julissagarcia@pursuit.org>
2023-12-20 22:58:20 -0500

    modified Result.jsx

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,15 +1,5 @@
-const Result = ({ nums, newCalc }) => {
-    // newCalc.sum is the initial value for the sum
-    const initialSumValue = newCalc && newCalc.sum ? newCalc.sum : 0;
-    // Use .reduce() to calculate the sum of elements in the 'nums' array, starting with 'initialSumValue'
-
-    const sum = nums.reduce((acc, curr) => acc + curr, initialSumValue);
-
-    return (
-        <p>{sum}</p>
-    );
+const Result = ({ result }) => {
+    return <p>{result}</p>;
 };
 
 export default Result;
-
-// can use .map here with result id from Form.jsx
