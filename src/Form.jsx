b899790669fabab1529e9744fc0824bf2df7c1bb
juligarc91 <julissagarcia@pursuit.org>
2023-12-20 22:57:01 -0500

    modified Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,89 +1,115 @@
-import React from "react";
 import { useState } from "react";
-import Result from "./Result"
+import Result from "./Result";
 import "./Form.css";
 
-
-
-
 function Form() {
-  const [newCalc, setNewCalc] = useState ({
-    numInput: [],
-    sum: 0,
-    average: 0,
-    mode: 0,
-  })
+  const [newCalc, setNewCalc] = useState({
+    numInput: "",
+    operation: "",
+  });
 
-  const [nums, setNums] = useState([])
+  const [result, setResult] = useState("");
 
   const handleChange = (event) => {
-    console.log(event.target.id, event.target.value)
-    setNewCalc({...newCalc, [event.target.id]: event.target.value })
-  }
-// draft 2
+    /* Note: spread operator (...) copy all existing key-value pairs from newCalc. Then, it updates the value of the key that matches event.target.name (either numInput or operation) with the new value from event.target.value. This is how you can update individual properties of an object in the state without losing the other properties. */
+    setNewCalc({ ...newCalc, [event.target.name]: event.target.value });
+  };
+
+  const calculateResult = (numArray, operation) => {
+  
+    switch (operation) {
+      // Evaluating the 'operation' parameter to determine which case to execute.
+  
+      case 'sum':
+        // Case for when the operation is 'sum'.
+        return numArray.reduce((acc, curr) => acc + curr, 0);
+        // .reduce method to sum up all elements in numArray.
+  
+      case 'average':
+        // Case for when the operation is 'average'.
+        return numArray.length 
+               ? numArray.reduce((acc, curr) => acc + curr, 0) / numArray.length 
+               : 0;
+        // If numArray is not empty, calculate the average:
+        // Sum all elements (like in the 'sum' case) and divide by the number of elements (numArray.length).
+        // If numArray is empty, return 0.
+  
+      case 'mode':
+        // Case for when the operation is 'mode'.
+        const frequency = {};
+        // Create an empty object to keep track of the frequency of each number.
+  
+        let maxFreq = 0;
+        // Initialize maxFreq, which will store the highest frequency found.
+  
+        numArray.forEach(num => {
+          // Iterate through each number in numArray.
+          frequency[num] = (frequency[num] || 0) + 1;
+          // Increment the frequency count for each number in the frequency object.
+          // If the number doesn't exist in frequency, use 0 as the initial value.
+  
+          if (frequency[num] > maxFreq) {
+            // Check if the current number's frequency is greater than the max frequency found so far.
+            maxFreq = frequency[num];
+            // If it is, update maxFreq with this new frequency.
+          }
+        });
+  
+        let modes = [];
+        // Initialize an array to store the mode(s).
+  
+        for (let num in frequency) {
+          // Iterate through each key (number) in the frequency object.
+          if (frequency[num] === maxFreq) {
+            // Check if the frequency of the current number is equal to maxFreq.
+            modes.push(Number(num));
+            // If it is, add this number to the modes array.
+          }
+        }
+  
+        return modes.length === 1 
+               ? modes[0] 
+               : `All inputted numbers appear with the same frequency.`;
+        // If there's only one mode, return it. 
+        // If not, return a message indicating all numbers appear with the same frequency.
+        // This case handles when there are no modes or multiple modes.
+  
+        break;
+        // Break statement to end the 'mode' case.
+  
+      default:
+        // Default case for when the operation doesn't match any of the specified cases.
+        return 'Invalid input.';
+        // Return a message indicating the input is invalid.
+    }
+  };
+  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const numArray = newCalc.numInput.split(',').map(num => Number(num.trim()));
+    const calculatedResult = calculateResult(numArray, newCalc.operation);
+    setResult(calculatedResult);
+  };
 
-    // Calculating sum, average, or mode based on the selected operation
-    let result;
-    switch (newCalc.operation) {
-        case 'sum':
-            result = numArray.reduce((acc, curr) => acc + curr, 0);
-            break;
-        case 'average':
-            result = numArray.length ? numArray.reduce((acc, curr) => acc + curr, 0) / numArray.length : 0;
-            break;
-        case 'mode':
-            break;
-        default:
-            result = 0;
-    }
-
-    setNewCalc({
-        ...newCalc,
-        sum: result,
-        // Add average and mode properties probably
-    });
-    setNums([numArray, ...nums]);
-    reset();
-}
-
-  // const handleSubmit = (e) => {
-  //   {/* might need a switch statement/ ternary/ or conditional to get either sum, avg, or mode and iterating using .reduce or .map() */}
-  //   const numArray = newCalc.numInput.split(',').map(num => Number(num.trim()));
-  //   const sum = newCalc.sum;
-  //   e.preventDefault();
-  //   setNums([numArray, ...nums]);
-  //   reset();
-  // }
-const reset = () => {
-  setNewCalc({
-    numInput: [],
-    sum: 0,
-    average: 0,
-    mode: 0,
-  })
-}
-
+  const reset = () => {
+    setNewCalc({ numInput: "", operation: "" });
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} value={newCalc.numInput} id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+        <input onChange={handleChange} value={newCalc.numInput} name="numInput" type="text" />
+        <select onChange={handleChange} value={newCalc.operation} name="operation">
           <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
+          <option value="sum">Sum</option>
+          <option value="average">Average</option>
+          <option value="mode">Mode</option>
         </select>
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p>
-          {/* {syntax on next line is correct but commented out so I can fix the component fx} */}
-          {/* <Result result={nums} newCalc={newCalc}/> */}
-        </p>
+        <Result result={result} />
       </section>
     </>
   );
